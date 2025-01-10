c560cf36570fc88bc72aab05ba4a6862c10a3fcf
LarsPeuling <lpeuling@gmail.com>
2025-01-10 10:09:19 +0100

    Add project files.

---
+++ ACRE_Create_Casus/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ACRE_Create_Casus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

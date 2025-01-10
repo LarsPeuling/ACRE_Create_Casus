7dc0839995e088af9177eacef715410aa562ba85
LarsPeuling <lpeuling@gmail.com>
2025-01-10 12:53:00 +0100

    Setup DB etc

--- ACRE_Create_Casus/Models/User.cs
+++ ACRE_Create_Casus/Models/User.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using ACRE_Create_Casus.DAL;
 
 namespace ACRE_Create_Casus.Models
 {
@@ -15,5 +17,30 @@ namespace ACRE_Create_Casus.Models
         {
             Role = role;
         }
+
+
+        //DataAccesLayer stuff
+        DataAccesLayer dal = new DataAccesLayer();
+        //Create observation
+        public Observation CreateObservation(DateTime date, int userId, bool isAnimal, string description)
+        {
+            Observation observation = new Observation(date, userId, isAnimal, description);
+            return dal.CreateObservation(observation);
+        }
+
+        //Create animal
+        public Animal CreateAnimal(int id, string name, string location, string photo)
+        {
+            Animal a = new Animal(id, name, location, photo);
+            return dal.CreateAnimal(a);
+        }
+
+
+        //Create plant
+        public Plant CreatePlant(int id, string name, string location, string photo)
+        {
+            return new Plant(id, name, location, photo);
+        }
+
     }
 }
